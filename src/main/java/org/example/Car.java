03051ac01ce5b18a8ce27e2480e4d53c09b0c005
wasabikun1704 <sbk283@naver.com>
2023-09-20 09:46:28 +0900

    first

---
+++ src/main/java/org/example/Car.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Car {
+    int speed;
+
+    static void run() {
+        System.out.println("달린다.");
+    }
+
+    static  void crash() {
+        System.out.println("사고났다.");
+    }
+}
+
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+// 문제 : 자동차 객체마다 서로 다른 최고속력를 가지도록 해주세요.
+
+class Main {
+    public static void main(String[] args) {
+        Car car = new Car();
+
+        car.run();
+        car.crash();
+    }
+}
